b9dcc7905b5e56200f0b603be8574df766219a5b
Maciej <rozek@o2.pl>
2024-06-07 16:05:12 +0200

    update

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Numbers } from './Numbers'
+import { RandomColor } from './RandomColor'
+
+export const App = () => {
+  return (
+    <>
+      <Numbers />
+      <RandomColor />
+    </>
+  )
+}
---
+++ src/hooks/useNumbers.ts
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+export const useNumbers = () => {
+  const [numbers, setNumbers] = useState<number[]>([])
+  const generateNumber = () => {
+    const newNumber = Math.round(Math.random() * 10000)
+    setNumbers((prevNumbers) => [...prevNumbers, newNumber])
+  }
+  const deleteNumber = (indexToDelete: number) => {
+    setNumbers((prevNumbers) =>
+      prevNumbers.filter((_, index) => index !== indexToDelete),
+    )
+  }
+  return {
+    numbers,
+    generateNumber,
+    deleteNumber,
+  }
+}
